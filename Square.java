72a9d090661e00de53b67c1e04cc74ef59edb7dc
Icamdaf <icampoltek@gmail.com>
2023-11-09 08:13:12 +0700

    2

---
+++ Square.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Square {
+
+    public static void main(String[] args) {
+     Scanner Scan20 = new Scanner(System.in);
+
+   System.out.print("Masukkan nilai n = ");
+   int N = Scan20.nextInt();
+   for (int i = 1; i <= N; i++) {
+    System.out.print("*");
+   }
+   
+   
+   
+    }
+}
--- Star.java
+++ Star.java
@@ -7,7 +7,7 @@ public class Star {
 System.out.print("Masukkan Nilai N = ");
 int N = Scan20.nextInt();
 
-for (int i = 1 ; i < N; i++) {
+for (int i = 0 ; i > N; i++) {
     System.out.print("*");
 }
 
