79f00c3722ec9c041a0028e299e05e2d69a7fada
Icamdaf <icampoltek@gmail.com>
2023-11-09 09:40:48 +0700

    11

--- NestedLoop_Nim.java
+++ NestedLoop_Nim.java
@@ -1,20 +1,24 @@
 import java.util.Scanner;
-/**
- * NestedLoop_Nim
- */
-public class NestedLoop_Nim {
 
+public class NestedLoop_Nim {
     public static void main(String[] args) {
-        Scanner Scan20 = new Scanner(System.in);
-        int temps;
+        Scanner input = new Scanner(System.in);
+        double[][] temps = new double[5][7];
+
         for (int i = 0; i < temps.length; i++) {
-            System.out.println("kote ke- " + i);
-            for (int j = 0; j < temps [0].length; j++) {
-                System.out.print("hari ke -" + (j+1) + ": ");
-                temps [i] [j] = Scan20.nextDouble;
+            System.out.println("Kota ke-" + i);
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
+                temps[i][j] = input.nextDouble();
             }
             System.out.println();
         }
-                  
-    }
+        for (int i = 0; i < temps.length; i++) {
+            System.out.print("Kota ke-" + (i + 1) + ": ");
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print(temps[i][j] + " ");
+            }
+            System.out.println();
+ }
+}
 }
