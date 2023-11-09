4dd66b5dd8c8fedb89007fa5d497e397ded5dbdf
Icamdaf <icampoltek@gmail.com>
2023-11-09 07:53:01 +0700

    1

---
+++ Star.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Star {
+
+    public static void main(String[] args) {
+    Scanner Scan20 = new Scanner(System.in);    
+    
+System.out.print("Masukkan Nilai N = ");
+int N = Scan20.nextInt();
+
+for (int i = 1 ; i < N; i++) {
+    System.out.print("*");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
