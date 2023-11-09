5b940c3f4217ab02275020dce8e58e653b14413d
Icamdaf <icampoltek@gmail.com>
2023-11-09 08:31:21 +0700

    4

--- Square.java
+++ Square.java
@@ -7,7 +7,7 @@ public class Square {
    System.out.print("Masukkan nilai n = ");
    int N = Scan20.nextInt();
   for ( int iOuter = 1; iOuter <= N; iOuter++ ) {
-     for (int i = 1; i <= N; i++) {
+     for (int i = 0; i <= N; i++) {
     System.out.print("*");
    }
 System.out.println();
